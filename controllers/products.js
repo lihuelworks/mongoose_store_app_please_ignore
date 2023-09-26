b8ee6a2ef71e92313af491406a5b8ba46a00bc31
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-09-26 05:43:53 -0300

    added queies to allProducts controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,49 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
     /* throw new Error('testing async errors') */
-    res.status(200).json({ msg: 'products testing route' })
+    const products = await Product.find({ featured: true }).sort('-name price')
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'products route' })
+    const { featured, company, name, sort, fields } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+
+    /* console.log(queryObject) */
+    let result = Product.find(queryObject)
+
+    /* sort */
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+
+    /* fields */
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        /* console.log(fieldsList) */
+        result = result.select(fieldsList)
+    }
+
+    const products = await result
+    res.status(200).json({ products, nbHits: products.length })
 }
+
+
 
 module.exports = {
     getAllProducts,

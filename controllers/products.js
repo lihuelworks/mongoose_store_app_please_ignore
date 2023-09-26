dd4ad4261b3944b8ca1a357064d107c031c19461
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-09-26 06:40:48 -0300

    added pagination and numeric filtering

--- controllers/products.js
+++ controllers/products.js
@@ -7,8 +7,10 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, fields } = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query
     const queryObject = {}
+
+    /* query resolution */
 
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
@@ -21,8 +23,35 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i' }
     }
 
-    /* console.log(queryObject) */
+
+
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+        let filters = numericFilters.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+        );
+
+        const options = ['price', 'rating']
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-');
+            if (options.includes(field)) {
+                queryObject[field] = { [operator]: Number(value) };
+            }
+        });
+    }
+
     let result = Product.find(queryObject)
+
+    /* result manipulation */
 
     /* sort */
     if (sort) {
@@ -38,6 +67,14 @@ const getAllProducts = async (req, res) => {
         /* console.log(fieldsList) */
         result = result.select(fieldsList)
     }
+
+
+
+    /* paging logic */
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit;
+    result = result.skip(skip).limit(limit)
 
     const products = await result
     res.status(200).json({ products, nbHits: products.length })
